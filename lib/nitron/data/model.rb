8ee91d2cdb5093033323ebb376c98896e3528c02
Matt Brewer <matt.brewer@me.com>
2012-06-21 13:42:24 -0400

    Added "find_all_by_" dynamic finder

--- lib/nitron/data/model.rb
+++ lib/nitron/data/model.rb
@@ -41,6 +41,9 @@ module Nitron
         if method.start_with?("find_by_")
           attribute = method.gsub("find_by_", "")
           relation.where("#{attribute} = ?", *args).first
+        elsif method.start_with?("find_all_by_")
+          attribute = method.gsub("find_all_by_", "")
+          relation.where("#{attribute} = ?", *args).to_a
         else
           super
         end
